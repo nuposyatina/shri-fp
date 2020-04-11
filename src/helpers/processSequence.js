de8640252a1660d8fbb0cb48629164cab8d424aa
Anna Tsukanova <at@indotech.ru>
2020-04-11 23:54:20 +0300

    Добавила валидацию строки

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -1,3 +1,4 @@
+
 /**
  * @file Домашка по FP ч. 2
  * 
@@ -15,37 +16,61 @@
  * Ответ будет приходить в поле {result}
  */
 import Api from '../tools/api';
+const R = require('ramda');
 
 const api = new Api();
 
-/**
- * Я – пример, удали меня
- */
-const wait = time => new Promise(resolve => {
-    setTimeout(resolve, time);
-})
+const validateString = async (str) => {
+    const lessThanTenSymbols = R.compose(
+        R.lt(R.__, 10),
+        R.length
+    );
+    const moreThanTwoSymbols = R.compose(
+        R.gt(R.__, 2),
+        R.length
+    );
+    const numIsPositive = R.compose(
+        R.gt(R.__, 0),
+        parseFloat
+    );
+    const onlyNumbers = R.compose(
+        R.gt(R.__, 0),
+        R.length,
+        R.match(/^[0-9]+\.{0,1}[0-9]*$/)
+    );
+    const isValid = R.allPass([lessThanTenSymbols, moreThanTwoSymbols, numIsPositive, onlyNumbers]);
+    return isValid(str) ? str : Promise.reject('ValidationError');
+};
 
-const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-    /**
-     * Я – пример, удали меня
-     */
-    writeLog(value);
+const getNum = async (str) => R.compose(
+    Math.round,
+    parseFloat
+)(str);
 
-    api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-        writeLog(result);
-    });
+const processSequence = async ({value, writeLog, handleSuccess, handleError}) => {
+    const logValue = async (value) => R.tap(writeLog, value);
+    const process = R.composeP(
+        logValue,
+        getNum,
+        validateString,
+        logValue
+    );
+    process(value).catch(err => handleError(err));
+    // api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
+    //     writeLog(result);
+    // });
 
-    wait(2500).then(() => {
-        writeLog('SecondLog')
+    // wait(2500).then(() => {
+    //     writeLog('SecondLog')
 
-        return wait(1500);
-    }).then(() => {
-        writeLog('ThirdLog');
+    //     return wait(1500);
+    // }).then(() => {
+    //     writeLog('ThirdLog');
 
-        return wait(400);
-    }).then(() => {
-        handleSuccess('Done');
-    });
+    //     return wait(400);
+    // }).then(() => {
+    //     handleSuccess('Done');
+    // });
 }
 
 export default processSequence;
