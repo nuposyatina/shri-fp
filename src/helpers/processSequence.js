14c1c92daaa7624648fc57c3d6cde7fc7da34652
Anna Tsukanova <at@indotech.ru>
2020-04-12 21:06:59 +0300

    Доделала 2 задание

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -20,57 +20,79 @@ const R = require('ramda');
 
 const api = new Api();
 
-const validateString = async (str) => {
-    const lessThanTenSymbols = R.compose(
-        R.lt(R.__, 10),
-        R.length
-    );
-    const moreThanTwoSymbols = R.compose(
-        R.gt(R.__, 2),
-        R.length
-    );
-    const numIsPositive = R.compose(
-        R.gt(R.__, 0),
-        parseFloat
-    );
-    const onlyNumbers = R.compose(
-        R.gt(R.__, 0),
-        R.length,
-        R.match(/^[0-9]+\.{0,1}[0-9]*$/)
-    );
-    const isValid = R.allPass([lessThanTenSymbols, moreThanTwoSymbols, numIsPositive, onlyNumbers]);
-    return isValid(str) ? str : Promise.reject('ValidationError');
-};
+const lessThanTenSymbols = R.compose(
+    R.lt(R.__, 10),
+    R.length
+);
+
+const moreThanTwoSymbols = R.compose(
+    R.gt(R.__, 2),
+    R.length
+);
+
+const numIsPositive = R.compose(
+    R.gt(R.__, 0),
+    parseFloat
+);
+
+const onlyNumbers = R.compose(
+    R.gt(R.__, 0),
+    R.length,
+    R.match(/^[0-9]+\.{0,1}[0-9]*$/)
+);
+
+const isValid = R.allPass(
+    [lessThanTenSymbols, moreThanTwoSymbols, numIsPositive, onlyNumbers]
+);
+
+const validateString = async (str) => (
+    isValid(str) ? str : Promise.reject('ValidationError')
+);
 
 const getNum = async (str) => R.compose(
     Math.round,
     parseFloat
 )(str);
 
+const transformNum = async (num) => (
+    api.get("https://api.tech/numbers/base", {
+        from: 10,
+        to: 2,
+        number: num,
+    })
+    .then(({ result }) => result)
+);
+
+const sqrNum = async (num) => Math.pow(num, 2);
+
+const getRemainderOfThree = R.modulo(R.__, 3);
+
+const getAnimal = async (num) => (
+    api.get(`https://animals.tech/${num}`, {})
+    .then(({result}) => result)
+);
+
+const applyValue = (fn) => async (value) => R.tap(fn, value);
+
 const processSequence = async ({value, writeLog, handleSuccess, handleError}) => {
-    const logValue = async (value) => R.tap(writeLog, value);
     const process = R.composeP(
-        logValue,
+        applyValue(writeLog),
+        getAnimal,
+        applyValue(writeLog),
+        getRemainderOfThree,
+        applyValue(writeLog),
+        sqrNum,
+        applyValue(writeLog),
+        R.length,
+        applyValue(writeLog),
+        transformNum,
+        applyValue(writeLog),
         getNum,
         validateString,
-        logValue
+        applyValue(writeLog)
     );
+
     process(value).catch(err => handleError(err));
-    // api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-    //     writeLog(result);
-    // });
-
-    // wait(2500).then(() => {
-    //     writeLog('SecondLog')
-
-    //     return wait(1500);
-    // }).then(() => {
-    //     writeLog('ThirdLog');
-
-    //     return wait(400);
-    // }).then(() => {
-    //     handleSuccess('Done');
-    // });
 }
 
 export default processSequence;
